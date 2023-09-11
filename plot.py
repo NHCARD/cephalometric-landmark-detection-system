a95b81d633f4718d2f8e4527fa1ff20b40f0f112
nhcard <inuri64@icloud.com>
2023-09-11 12:54:35 +0900

    scatter_edit btn fix list item remove canvas size change

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import glob
+import os
 import sys
 import time
 
@@ -14,6 +15,10 @@ import net_hand as network
 import torch
 from func import argsoftmax
 import subprocess
+import openpyxl
+
+H = 800
+W = 640
 
 
 class MyWindow(QWidget):
@@ -38,15 +43,20 @@ class MyWindow(QWidget):
         self.fileDir = None
         self.initUI()
         self.setLayout(self.layout)
-        self.setGeometry(200, 200, 800, 600)  # 창의 위치 x좌표, y좌표, 가로크기, 세로 크기
+        self.setGeometry(200, 200, 1400, 800)  # 창의 위치 x좌표, y좌표, 가로크기, 세로 크기
 
     def initUI(self):
-        self.vis_output = plt.Figure()  # 모델 아웃풋 이미지
-        self.vis_origin = plt.Figure()  # 모델 인풋 이미지
+        self.vis_output = plt.Figure(figsize=(10, 10))  # 모델 아웃풋 이미지
+        self.vis_origin = plt.Figure(figsize=(5, 5))  # 모델 인풋 이미지
         self.canvas = FigureCanvas(self.vis_output)  # 아웃풋 이미지 출력 박스
         self.canvas2 = FigureCanvas(self.vis_origin)  # 인풋 이미지 출력 박스
 
-        layout = QHBoxLayout()  # 메인 레이아웃 (Horizen)
+        menubar = QMenuBar(self)
+        # menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('&Menu')
+        # filemenu.
+
+        layout = QHBoxLayout()  # 메인 레이아웃 (Horizon)
         layout.addWidget(self.canvas2)
         layout.addWidget(self.canvas)
 
@@ -63,12 +73,15 @@ class MyWindow(QWidget):
 
         self.file_list = QListWidget(self)
         self.file_list.itemDoubleClicked.connect(self.List_click)
+        self.del_list = QPushButton('Delete')
+        self.del_list.clicked.connect(self.Delete_list)
 
         btn_layout.addWidget(load_img_btn)
 
         btn_layout.addWidget(directory_btn)
         btn_layout.addWidget(edit_btn)
         btn_layout.addWidget(self.file_list)
+        btn_layout.addWidget(self.del_list)
 
         layout.addLayout(btn_layout)
 
@@ -83,7 +96,9 @@ class MyWindow(QWidget):
             self.orig_H = img.shape[0]
             self.orig_W = img.shape[1]
 
-            self.file_list.addItem(self.fileDir)
+            find_idx = self.fileDir.rindex('/')
+            # print(f'fine_idx : {find_idx}')
+            self.file_list.addItem(self.fileDir[find_idx + 1:])
 
             # img = cv2.resize(img, (800, 640))
 
@@ -114,7 +129,7 @@ class MyWindow(QWidget):
                                         batch_size=1,
                                         shuffle=False, num_workers=5)
 
-            f_list = glob.glob(self.fileDir + '/*.png')
+            f_list = os.listdir(self.fileDir)
             for i in f_list:
                 self.file_list.addItem(f'{i}')
 
@@ -131,20 +146,17 @@ class MyWindow(QWidget):
             pass
 
     def predict(self):
+
         Ymap, Xmap = np.mgrid[0:H:1, 0:W:1]
         Ymap, Xmap = torch.tensor(Ymap.flatten(), dtype=torch.float).unsqueeze(1).to(self.device_txt), \
             torch.tensor(Xmap.flatten(), dtype=torch.float).unsqueeze(1).to(self.device_txt)
-        ind = torch.cat([Xmap / W, Ymap / H], dim=1)
 
         with torch.no_grad():
-            ss_time = time.time()
-            for inputs, size, asdf in self.test_data:
-                ee_time = time.time()
+            for inputs, size in self.test_data:
 
                 inputs = inputs.to(self.device_txt)
 
                 outputs = self.model(inputs)
-                print(f'asdfasdf : {ee_time - ss_time}')
                 # print("outputs.shape : ", outputs.shape)
                 pred = torch.cat([argsoftmax(outputs[0].view(-1, H * W), Ymap, beta=1e-3) * (self.orig_H / H),
                                   argsoftmax(outputs[0].view(-1, H * W), Xmap, beta=1e-3) * (self.orig_W / W)],
@@ -159,35 +171,44 @@ class MyWindow(QWidget):
 
             self.vis_output.clear()
             self.vis_origin.clear()
-            self.ax = self.vis_output.add_subplot(111)
+
+            self.pred_plot = self.vis_output.add_subplot(111)
             self.origin_plot = self.vis_origin.add_subplot(111)
-            self.ax.axis('off')
-            self.ax.imshow(self.inputs_resize, cmap='gray')
+
+            self.pred_plot.axis('off')
+            self.pred_plot.imshow(self.inputs_resize, cmap='gray')
+
             self.origin_plot.axis('off')
             self.origin_plot.imshow(self.inputs_resize, cmap='gray')
-            # ax.subplots_adjust(left=0)
 
             pred = pred.detach().cpu().numpy()
 
-            for i in pred:
-                self.ax.scatter(int(i[1]), int(i[0]), s=20, marker='.', c='b')
+            for idx, i in enumerate(pred):
+                self.pred_plot.scatter(int(i[1]), int(i[0]), s=20, marker='.', c='b')
+
+                self.pred_plot.text(i[1] + 0.02, i[0] + 0.02, f'{idx + 1}', c='b', fontsize=7)
 
             self.canvas.draw()
             self.canvas2.draw()
 
     def edit_scatter(self):
         if self.fileDir != None:
-            self.vis_output, self.ax = plt.subplots(figsize=(12, 10))
-            plt.imshow(self.inputs, cmap='gray')
+            current_num = self.file_list.currentRow()
+            self.edit_output, self.edit_plot = plt.subplots(figsize=(12, 10))
+            plt.imshow(self.input[current_num], cmap='gray')
+
+            for y, x in self.pred[current_num]:
+                self.edit_plot.scatter(int(x), int(y), s=20, marker='.', c='b')
+
             plt.xlabel('x')
             plt.ylabel('y')
             plt.title('Interactive Plot')
 
-            self.ax.set_aspect('auto', adjustable='box')
+            self.edit_plot.set_aspect('auto', adjustable='box')
 
             self.xdata = [0]
             self.ydata = [0]
-            self.line, = self.ax.plot(self.xdata, self.ydata)
+            self.line, = self.edit_plot.plot(self.xdata, self.ydata)
 
             cid = plt.connect('button_press_event', self.add_point)
             plt.tight_layout()
@@ -196,7 +217,7 @@ class MyWindow(QWidget):
             QMessageBox.about(self, 'Notice', 'Plese Load IMG.')
 
     def add_point(self, event):
-        if event.inaxes != self.ax:
+        if event.inaxes != self.edit_plot:
             return
 
         if event.button == 1:
@@ -212,7 +233,7 @@ class MyWindow(QWidget):
         if event.button == 3:
             self.xdata.pop()
             self.ydata.pop()
-            self.vis_output.figimage(self.inputs, cmap='gray', resize=True)
+            self.edit_output.figimage(self.inputs, cmap='gray', resize=True)
 
             plt.scatter(self.xdata, self.ydata, s=20, marker='.', c='b')
             plt.draw()
@@ -220,23 +241,48 @@ class MyWindow(QWidget):
     def List_click(self):
         if self.fileDir != None:
             num = self.file_list.currentRow()
-            print(len(self.input))
+            # print(self.file_list.currentItem().text())
+            # print(num)
+            # print(len(self.input))
 
             self.vis_output.clear()
             self.vis_origin.clear()
-            self.ax = self.vis_output.add_subplot(111)
+            self.pred_plot = self.vis_output.add_subplot(111)
             self.origin_plot = self.vis_origin.add_subplot(111)
             self.origin_plot.axis('off')
-            self.ax.axis('off')
+            self.pred_plot.axis('off')
 
-            self.ax.imshow(self.input[num], cmap='gray')
+            self.pred_plot.imshow(self.input[num], cmap='gray')
             self.origin_plot.imshow(self.input[num], cmap='gray')
 
-            for i in self.pred[num]:
-                self.ax.scatter(int(i[1]), int(i[0]), s=20, marker='.', c='b')
+            for idx, i in enumerate(self.pred[num]):
+                self.pred_plot.scatter(int(i[1]), int(i[0]), s=20, marker='.', c='b')
+                self.pred_plot.text(i[1] + 0.02, i[0] + 0.02, f'{idx + 1}', c='b', fontsize=7)
 
             self.canvas.draw()
             self.canvas2.draw()
+
+    def Delete_list(self):
+        del_num = self.file_list.currentRow()
+        self.file_list.takeItem(del_num)
+        self.input.pop(del_num)
+        self.pred.pop(del_num)
+
+        # 현재 선택된 표시된 이미지 출력했을 때 다음 이미지로 뜨개 처리
+
+    def excel_save(self):
+        self.savedir = QFileDialog.getExistingDirectory(self, "Open Directory", r'./')
+        write_wb = openpyxl.Workbook()
+
+        write_ws = write_ws = write_wb.create_sheet('Sheet1')
+        write_ws = write_wb.active
+        for i in range(1, 38):
+            write_ws[f'{str(i)}']
+        # write_ws['B1'] = 'Sella'
+        write_ws['A2'] = self.file_list.currentItem().text()
+        write_ws['B2'] = 0
+
+        write_wb.save('./test.xlsx')
 
 
 if __name__ == "__main__":
