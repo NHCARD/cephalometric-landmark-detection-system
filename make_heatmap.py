4ccc11c7be000df3b9cc18bfe4db5a1732b00dc3
yj <wku.yjjeong@gmail.com>
2023-12-12 22:13:04 +0900

    chore

--- make_heatmap.py
+++ make_heatmap.py
@@ -4,10 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
-h = 2400
-w = 2880
+h, w = 2400, 2880
 std = 10
-
 
 heatmap = np.zeros((h, w))
 coordinateMtx = np.zeros((h, w, 2))
@@ -15,39 +13,31 @@ coordinateMtx[:, :, 0] = np.tile(np.arange(h).reshape(h, 1), (1, w))
 coordinateMtx[:, :, 1] = np.tile(np.arange(w), (h, 1))
 
 
-def Laplace(center, sigmaD=None, __coordinateMtx=None):  # Gaussian Filter
-    centerX = center[0]
-    centerY = center[1]
-    centerMtx = np.ones((h, w, 2))
-    centerMtx[:, :, 0] *= centerY
-    centerMtx[:, :, 1] *= centerX
+def Laplace(center, sigmaD=None):  # Gaussian Filter
+    center_x, center_y = center
+    center_mtx = np.ones((h, w, 2))
+    center_mtx[:, :, 0] *= center_y
+    center_mtx[:, :, 1] *= center_x
 
-    dist = np.sum(np.abs(coordinateMtx - centerMtx), axis=2)
+    distance = np.sum(np.abs(coordinateMtx - center_mtx), axis=2)
 
-    SDMap = (1 / (2 * sigmaD)) * np.exp(-np.abs(dist) / (2 * sigmaD) ** 2)
+    SDMap = (1 / (2 * sigmaD)) * np.exp(-np.abs(distance) / (2 * sigmaD) ** 2)
     SDMap = SDMap / np.max(SDMap)
 
     return SDMap
 
 
-file = open(r'./train-gt.json')
-jsonstring = json.load(file)
-points = jsonstring.get('points')
-
-sdmap = Laplace((points[0]['point'][:2]), sigmaD=std, __coordinateMtx=coordinateMtx, img=heatmap)
-Heatmap = np.maximum(sdmap, heatmap)
-plt.imsave(f'./test_std10.png', Heatmap * 255, cmap='gray')
-
 if __name__ == "__main__":
+    with open('./train-gt.json') as file:
+        json_data = json.load(file)
+        points = json_data.get('points')
+
     for i in range(1, 39):
-        try:
-            os.mkdir(f'./heatmap/1{i + 1:0>2d}')
-        except:
-            pass
+        os.makedirs(f'./heatmap/1{i + 1:0>2d}', exist_ok=True)
 
     for i in range(0, 100):
         print(i)
         for j in range(38):
-            sdmap = Laplace((points[(i * 38) + j]['point'][:2]), sigmaD=std, __coordinateMtx=coordinateMtx, img=heatmap)
+            sdmap = Laplace((points[i * 38 + j]['point'][:2]), sigmaD=std)
             Heatmap = np.maximum(sdmap, heatmap)
             plt.imsave(f'./heatmap/1{j+1:0>2d}/{i+1:0>3d}.png', Heatmap * 255, cmap='gray')
